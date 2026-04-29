d20d93b9b78cf5ea17eb17875312b05fa3c7c7b8
jfuenzalidaleal <joaquin.fuenzalida14@gmail.com>
2026-04-28 20:59:33 -0400

    Initial Commit

---
+++ src/main/java/com/grupo_cordillera/microservicio_kpi/MicroservicioKpiApplication.java
@@ -0,0 +1,13 @@
+package com.grupo_cordillera.microservicio_kpi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioKpiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioKpiApplication.class, args);
+	}
+
+}
